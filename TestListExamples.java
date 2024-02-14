3db2c593667e40ede1ae504c923bc42fa8dde1d2
flynn5627 <156235257+flynn5627@users.noreply.github.com>
2024-02-14 10:48:18 -0800

    Complete task

--- TestListExamples.java
+++ TestListExamples.java
@@ -18,4 +18,13 @@ public class TestListExamples {
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
   }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "b", "d");
+    List<String> right = Arrays.asList("a", "c");
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
 }
--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH=".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar"
 
 rm -rf student-submission
 rm -rf grading-area
@@ -14,3 +14,38 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+cp student-submission/ListExamples.java grading-area
+cp *.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+if ! [[ -f ListExamples.java ]]
+then 
+    echo "Missing ListExamples.java in student submission"
+    echo "Score : 0"
+    exit 1
+fi
+
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then 
+    echo "Compilation error"
+    echo "Score: 0"
+    exit
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > ta-output.txt
+
+if [[ $? -eq 0 ]]
+then 
+    echo "Score: 100"
+    exit
+else
+    tests=$( cat ta-output.txt | tail -n 2 | head -n 1 | awk -F'[, ]' '{ print $3 }' )
+    failures=$( cat ta-output.txt | tail -n 2 | head -n 1 | awk -F'[, ]' '{ print $7 }' )
+    successes=$(( tests - failures ))
+    echo "Score: $successes out of $tests"
+fi
